e90f2c1da82a1edacb96fad9d016bcea3f96221e
unknown <student@AI251LAB0234P16.aicala.edmc.stu>
2015-01-24 11:08:14 -0800

    - NavMesh Navigation Added - NGUI included in project

---
+++ WDWDN/Assets/NavMeshMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshMovement : MonoBehaviour 
+{
+	NavMeshAgent agent;
+	public Camera petCam;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.GetMouseButtonDown(0) )
+		{
+			Ray ray = petCam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			RaycastHit[] hits = Physics.RaycastAll(ray, 10000.0f);
+
+			Debug.DrawRay( ray.origin, ray.direction * 10000.0f, Color.red );
+
+			if( hits.Length == 0 )
+				return;
+
+			RaycastHit firstHit = hits[hits.Length-1];
+
+			Debug.Log(firstHit.transform.name);
+
+			if( firstHit.transform.gameObject.layer == LayerMask.NameToLayer ("Ground") )
+			{
+				agent.SetDestination (firstHit.point);
+			}
+		}
+	}
+}
