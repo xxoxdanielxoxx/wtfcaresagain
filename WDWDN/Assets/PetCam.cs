17d3ac5962812c7cbe6f2c7e1e06e5f5421c6ab4
unknown <student@AI251LAB0234P16.aicala.edmc.stu>
2015-01-24 14:29:34 -0800

    - GUI prototype added - PetCam Script Added

--- WDWDN/Assets/PetCam.cs
+++ WDWDN/Assets/PetCam.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class PetCam : MonoBehaviour 
 {
 	public Transform target;
+	public int xMinBounds;
+	public int xMaxBounds;
+	public int zMaxBounds;
+	public int zMinBounds;
 
 	// Update is called once per frame
 	void Update () 
 	{
-		
 		transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z);
+
+		Vector3 temp = transform.position;
+
+		temp.x = Mathf.Clamp (temp.x, xMinBounds, xMaxBounds);
+
+		temp.z = Mathf.Clamp (temp.z, zMinBounds, zMaxBounds);
+
+		transform.position = temp;
 	}
 }
